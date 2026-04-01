14d0b1989cc22e37cf5c028f35168c7379d61676
Akshat Sharma <akshatksharma@wustl.edu>
2026-04-01 10:48:04 -0400

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.swift
@@ -0,0 +1,50 @@
+class Solution {
+
+    /*
+    breakdown
+    - water area from the max size container in heights
+    - water area formula for 2 heights i and j
+        A = L * W = min(h_i, h_j) * (abs(i - j))
+    
+    edge cases
+    - heights length - at least 2 heights always
+    - height magnitude -- can have 0 values
+
+    [1, 5, 0, 5, 6, 7]
+    [1, 5, 4, 3, 4]
+    [1, 2, 1]
+
+    approach
+    - can't sort
+    - brute force: for each height, look at all heights after and calc areas, keep track of max area
+        N^2
+    
+    if we find a taller height, we want to make that our container
+        i and j refer to heights of containers
+        we start at the start of the array and calc the area
+            then, if j's height > i's height, we set i to j (make it the starting height) and j -> j + 1
+                if j's height is < i's height we continue progressing j and calc the other areas
+    */
+
+    func maxArea(_ heights: [Int]) -> Int {
+        var maxHeight = 0
+        for (outerIdx, outerHeight) in heights.enumerated() {
+            for (innerIdx, innerHeight) in heights.enumerated() {
+                if innerIdx > outerIdx {
+                    maxHeight = max(maxHeight, calcHeight(outerIdx, innerIdx, heights))
+                }
+            }
+        }
+
+        return maxHeight
+    }
+
+    private func calcHeight(_ i: Int, _ j: Int, _ heights: [Int]) -> Int {
+        guard i < heights.count && j < heights.count else { return 0 }
+
+        let width = abs(i - j)
+        let height = min(heights[i], heights[j])
+
+        return width * height
+    }
+}
