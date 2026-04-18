24aa783ac444f4107eaee177b7f29b8835c262c9
Akshat Sharma <akshatksharma@wustl.edu>
2026-04-18 08:31:12 -0400

    Add: binary-search - submission-15

---
+++ Data Structures & Algorithms/binary-search/submission-15.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        var l = 0, r = nums.count - 1
+
+        if (nums[l] == target) { return l }
+        // if (nums[l] > target || nums[r] < target) { return -1 }
+
+        while r - l > 1 {
+            let mid = l + ((r - l) / 2)
+
+            if nums[mid] < target {
+                l = mid
+            } else {
+                r = mid
+            }
+        }
+
+        return nums[r] == target ? r : -1
+    }
+}
