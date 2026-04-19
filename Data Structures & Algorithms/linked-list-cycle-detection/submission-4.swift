855731f89f78d7fe87845d62f271d45b2d8dfdd1
Akshat Sharma <akshatksharma@wustl.edu>
2026-04-19 16:57:09 -0400

    Add: linked-list-cycle-detection - submission-4

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-4.swift
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     var val: Int
+ *     var next: ListNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+
+ /*
+
+            ||   |           
+ A - B - C - D - E
+
+ A
+
+ _
+
+ */
+
+class Solution {
+    func hasCycle(_ head: ListNode?) -> Bool {
+        var fast = head
+        var slow = head
+
+        while fast != nil, fast?.next != nil {
+            slow = slow?.next
+            fast = fast?.next?.next
+
+            if slow === fast {
+                return true
+            }
+        }
+
+        return false
+    }
+}
