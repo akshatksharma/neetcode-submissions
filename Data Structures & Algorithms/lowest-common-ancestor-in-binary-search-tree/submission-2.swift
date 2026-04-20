9e02c40903944d3c49c8c0308b5452aeab24fe77
Akshat Sharma <akshatksharma@wustl.edu>
2026-04-20 13:33:27 -0400

    Add: lowest-common-ancestor-in-binary-search-tree - submission-2

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-2.swift
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+
+    /*
+        look for ways to search through tree
+
+        if the node is greater than both then we need to search left
+        if node is less than both then we need to search right
+
+        if the node is in between both (closed boundary) then we're at the LCA
+
+        edge cases
+            - tree can't be nil
+            - p and q have to be in there
+
+    */
+
+    func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
+        guard let root, let p, let q else { return nil } // double check this
+
+        if root.val > p.val && root.val > q.val {
+            return lowestCommonAncestor(root.left, p, q)
+        } else if root.val < p.val && root.val < q.val {
+            return lowestCommonAncestor(root.right, p, q)
+        } else if (root.val >= q.val && root.val <= p.val) || (root.val <= q.val && root.val >= p.val) {
+            return root
+        }
+
+        return root
+    }
+}
