71b9b189d8f21f8bf094c5e5615fef1b74ef65b8
Akshat Sharma <akshatksharma@wustl.edu>
2026-04-21 00:28:32 -0400

    Add: level-order-traversal-of-binary-tree - submission-6

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-6.swift
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+        var answer = [[Int]]()
+        var queue = [TreeNode?]() // can impl as deque for O(1) pops and pushes
+
+        queue.append(root)
+
+        while !queue.isEmpty {
+            let levelSize = queue.count
+            var level = [Int]()
+
+            for _ in 0..<levelSize {
+                if let node = queue.removeFirst() {
+                    level.append(node.val)
+                    queue.append(node.left)
+                    queue.append(node.right)
+                }
+            }
+
+            if !level.isEmpty {
+                answer.append(level)
+            }
+        }
+
+        return answer
+    }
+}
