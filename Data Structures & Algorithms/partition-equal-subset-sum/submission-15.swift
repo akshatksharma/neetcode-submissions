f0706a9894da9f26165b8d824223a3a66ea97a48
Akshat Sharma <akshatksharma@wustl.edu>
2026-04-25 14:40:14 -0400

    Add: partition-equal-subset-sum - submission-15

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-15.swift
@@ -0,0 +1,67 @@
+class Solution {
+    func canPartition(_ nums: [Int]) -> Bool {
+        let total = nums.reduce(0) { $0 + $1 }
+        let target = total / 2
+
+        // memo
+        var memo: [[Bool?]] = Array(repeating: Array(repeating: nil, count: target + 1), count: nums.count + 1) // target + 1 so we can index target easily
+
+        guard total % 2 == 0 else { return false }
+
+        return canPartitionImpl(nums, target, 0, &memo)
+    }
+
+
+    func canPartitionImpl(_ nums: [Int], _ target: Int, _ index: Int, _ memo: inout [[Bool?]]) -> Bool {
+        // base cases
+        if target == 0 {
+            return true
+        }
+
+        if index >= nums.count {
+            return false
+        }
+
+        if let memoAns = memo[index][target] {
+            return memoAns
+        }
+
+
+        var canPartition = canPartitionImpl(nums, target, index + 1, &memo)
+        if target - nums[index] >= 0 {
+            canPartition = canPartition || canPartitionImpl(nums, target - nums[index], index + 1, &memo)
+        }
+
+        memo[index][target] = canPartition
+
+        return canPartition
+    }
+
+
+    /*
+                            F(target - nums[i], i + 1)
+        F(target, i) = {    OR
+                            F(target, i + 1)
+
+
+
+
+
+           [1, 2, 3, 4] -> target = 5
+
+        5
+            4
+                2
+
+            5
+
+                3
+
+                    0
+
+
+                5
+
+
+    */
+}
