a2a2acb7f81814c06e9fc671d8f52a74f3e0bb9b
Akshat Sharma <akshatksharma@wustl.edu>
2026-04-26 16:49:21 -0400

    Add: house-robber - submission-15

---
+++ Data Structures & Algorithms/house-robber/submission-15.swift
@@ -0,0 +1,30 @@
+class Solution {
+    func rob(_ nums: [Int]) -> Int {
+        guard nums.count > 1 else { return nums[0] }
+
+        var dp = Array(repeating: 0, count: nums.count)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in 2..<nums.count {
+            let pickNeighbor = dp[i-1]
+            let pickCurrent = nums[i] + dp[i-2]
+
+            dp[i] = max(pickNeighbor, pickCurrent)
+        }
+
+        return dp[nums.count - 1]
+    }
+
+    /*
+    [10000, 20, 4, 100]
+        F(i) = max amount of money picking i items
+
+            F(3) = max(20, 10 + 4)
+            F(i) = max(F(i-1), nums[i-1] + F(i-2))
+
+            i <= 0
+                -> 0
+
+    */
+}
