65792e910fe0befbd98bde5ab0b4fc328decae92
Akshat Sharma <akshatksharma@wustl.edu>
2026-04-26 20:39:29 -0400

    Add: count-paths - submission-11

---
+++ Data Structures & Algorithms/count-paths/submission-11.swift
@@ -0,0 +1,37 @@
+class Solution {
+    func uniquePaths(_ m: Int, _ n: Int) -> Int {
+        var lastRow: [Int] = Array(repeating: 0, count: n)
+        lastRow[n-1] = 1 // corner is 1
+
+        for i in stride(from: m-1, through: 0, by: -1) {
+            var currRow = Array(repeating: 0, count: n)
+            currRow[n-1] = 1 // corner is 1
+
+            for j in stride(from: n-2, through: 0, by: -1) {
+                currRow[j] += currRow[j+1] + lastRow[j]
+            }
+
+            lastRow = currRow
+        }
+
+        return lastRow[0]
+    }
+
+
+    /*
+    i indexes rows (0..<m)
+    j indexs columns (0..<n)
+
+    F(i,j) gives the number of paths to the bottom corner
+
+    F(i,j) = F(i+1,j) + F(i,j+1)
+        F(m-1,n-1) = 1
+        F(i,n) = 0
+        F(m, j) = 0
+
+    we can build a dp table of size m x n and work out way backwards to i = 0 and j = 0
+        time: O(n*m)
+        space: O(n*m)
+
+    */
+}
