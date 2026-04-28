5cb7c320e2ee0d5c54d3dd8c81935eeaa7712c4e
Akshat Sharma <akshatksharma@wustl.edu>
2026-04-27 21:30:15 -0400

    Add: word-break - submission-19

---
+++ Data Structures & Algorithms/word-break/submission-19.swift
@@ -0,0 +1,31 @@
+class Solution {
+    func wordBreak(_ s: String, _ wordDict: [String]) -> Bool {
+        let s = Array(s)
+        var dp: [Bool] = Array(repeating: false, count: s.count+1)
+        dp[s.count] = true
+
+        for i in stride(from: s.count-1, through: 0, by: -1) {
+            for word in wordDict {
+                if i+word.count <= s.count, word == String(s[i..<i+word.count]), dp[i+word.count] {
+                    dp[i] = true
+                }
+            }
+        }
+
+        return dp[0]
+    }
+
+    /*
+        size = 8
+
+        leetcode
+
+        leet,code
+
+        O(n*m) n is size of string and m is the size of the dictionary
+
+        F(i) = F(i + s.count)
+
+
+    */
+}
