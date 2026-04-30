5cd2313ad8128d361e5823d5fe7cf2cde5ee72ca
Akshat Sharma <akshatksharma@wustl.edu>
2026-04-29 22:52:08 -0400

    Add: course-schedule-ii - submission-6

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-6.swift
@@ -0,0 +1,67 @@
+class Solution {
+    func findOrder(_ numCourses: Int, _ prerequisites: [[Int]]) -> [Int] {
+        // make adj list
+        var adjList = [Int: [Int]]()
+        for course in 0..<numCourses {
+            adjList[course] = []
+        }
+        for prereq in prerequisites {
+            adjList[prereq[0], default: []].append(prereq[1])
+        }
+
+        var ans = [Int]()
+        var visiting = Set<Int>()
+        var finished = Set<Int>()
+
+        /* dfs on each elem, 
+            track which ones we're visiting
+            which one's we've finished visiting
+            path so far
+        */
+        func dfs(_ course: Int) -> Bool {
+            // base cases
+            if visiting.contains(course) {
+                return false
+            }
+
+            if finished.contains(course) {
+                return true
+            }
+            
+            let neighbors = adjList[course]! 
+
+            // recurse
+            visiting.insert(course)
+            for neighbor in neighbors {
+                if !dfs(neighbor) {
+                    return false
+                }
+            }
+            visiting.remove(course)
+            finished.insert(course)
+            ans.append(course)
+
+            return true
+        }
+
+        for course in 0..<numCourses {
+            if !dfs(course) {
+                return []
+            }
+        }
+
+        return ans
+    }
+
+    /*
+        [[0,1] [1, 2] [2, 3] [2, 4]]
+                    
+        0 -> 1 -> 2 -> 3
+                  -> 4 
+
+        [[0,1] [1,0]]
+
+            0<>1
+
+    */
+}
