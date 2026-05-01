283ea12549e8f71df1fc7cf151f4e94d7387b5ef
Akshat Sharma <akshatksharma@wustl.edu>
2026-04-30 22:01:44 -0400

    Add: valid-tree - submission-23

---
+++ Data Structures & Algorithms/valid-tree/submission-23.swift
@@ -0,0 +1,50 @@
+class Solution {
+    func validTree(_ n: Int, _ edges: [[Int]]) -> Bool {
+        var adjList = Array(repeating: Set<Int>(), count: n)
+        var visited = Set<Int>()
+
+        // build adjList
+        for edge in edges {
+            adjList[edge[0]].insert(edge[1])
+            adjList[edge[1]].insert(edge[0])
+        }
+
+        // dfs
+        func dfs(_ node: Int, _ parent: Int) -> Bool {
+            if visited.contains(node) {
+                return false
+            }
+
+            visited.insert(node)
+
+            let neighbors = adjList[node]
+
+            for neighbor in neighbors {
+                if neighbor == parent {
+                    continue
+                }    
+                if !dfs(neighbor, node) {
+                    return false
+                }
+            }
+
+            return true
+        }
+
+        // run dfs
+       let hasCycle = !dfs(0, -1)
+
+        return !hasCycle && visited.count == n
+    }
+
+
+    /*
+        tree == no cycles?
+
+        1. make a adjlist
+            need to add edges for both pairs
+        2. run dfs / kahn's and look for cycles, return false if cycle
+        3. return true if no cycles
+
+    */
+}
