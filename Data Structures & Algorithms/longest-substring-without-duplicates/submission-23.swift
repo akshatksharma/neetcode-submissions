6ff458496406ef5fa228c4562f4e471a866853a2
Akshat Sharma <akshatksharma@wustl.edu>
2026-05-03 15:37:06 -0400

    Add: longest-substring-without-duplicates - submission-23

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-23.swift
@@ -0,0 +1,38 @@
+class Solution {
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        guard !s.isEmpty else { return 0 }
+        guard s.count > 1 else { return 1 }
+
+        let s = Array(s)
+        var charSet = Set<Character>()
+
+        var L = 0
+        var R = 0
+        
+        var maxLength = 0
+
+        while R < s.count {
+            while charSet.contains(s[R]), L < R { // invalid substring
+                charSet.remove(s[L])
+                L += 1
+            }
+
+            charSet.insert(s[R]) // valid substring
+            maxLength = max(maxLength, R - L + 1)
+            R += 1
+        }
+
+        return maxLength
+    }
+
+    /*
+    brute force: build up and check substring ... O(n^2)
+
+    or we can build substrings starting from one end
+    when we add chars to substring, we add to a map
+    if we see a duplicate, we start to remove chars from substring and the map until that duplicate is gone
+    then we continue growing substring
+
+    this'll let us go through all the substrings and share the info about counts between them
+    */
+}
