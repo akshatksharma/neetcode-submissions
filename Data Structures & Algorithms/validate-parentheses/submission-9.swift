7761bbc5d17232486eb614047e0992cdcb105760
Akshat Sharma <akshatksharma@wustl.edu>
2026-05-04 23:24:10 -0400

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.swift
@@ -0,0 +1,33 @@
+class Solution {
+    func isValid(_ s: String) -> Bool {
+        var stack = [Character]()
+        var pairs: [Character: Character] = [
+            "(": ")",
+            "[": "]",
+            "{": "}"
+        ]
+
+        let chars = Array(s)
+        for char in chars {
+            switch char {
+                case "(", "[", "{":
+                    stack.append(char)
+                case ")", "]", "}":
+                    // check if it has a pair on the stack
+                    if let lastChar = stack.last, pairs[lastChar] == char {
+                        _ = stack.popLast()
+                    } else {
+                        return false 
+                    }
+                default:
+                    break
+            }
+        }
+
+        if !stack.isEmpty {
+            return false
+        }
+        
+        return true
+    }
+}
