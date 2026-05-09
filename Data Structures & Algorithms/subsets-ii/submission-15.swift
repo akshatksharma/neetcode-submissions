a24ea792d669b68ace2e0a464828a22e5c47aa4d
Akshat Sharma <akshatksharma@wustl.edu>
2026-05-09 19:40:42 -0400

    Add: subsets-ii - submission-15

---
+++ Data Structures & Algorithms/subsets-ii/submission-15.swift
@@ -0,0 +1,47 @@
+class Solution {
+    func subsetsWithDup(_ nums: [Int]) -> [[Int]] {
+        var nums = nums.sorted()
+        var subsets = Set<[Int]>()
+        var subset = [Int]()
+
+        // 1 1 2
+
+        func dfs(_ i: Int) {
+            if i == nums.count {
+                subsets.insert(subset)
+                return
+            }
+
+            subset.append(nums[i])
+            dfs(i+1)
+            _ = subset.popLast()
+
+            var j = i
+            while j < nums.count-1, nums[j] == nums[j+1] {
+                j += 1
+            }
+
+            dfs(j+1)
+        }
+
+        dfs(0)
+        return subsets.map { Array($0) }
+    }
+
+
+    /* 
+        what is a duplicate subset? it just cant have the same values as
+        another subset?
+
+        1 2 1 - we can choose to include to not include every item
+
+                           _                                  0
+                    1                 _                       1
+               1,2    1          2        _                   2
+        1,2,1   1,2  1,1  1   2,1  2    1    _                3
+                
+
+                1,2 == 2,1
+                1 == 1
+    */
+}
