e7291b53743d4a4c6a0492fceb46ef1f5dcd6c61
Akshat Sharma <akshatksharma@wustl.edu>
2026-05-08 21:37:13 -0400

    Add: binary-tree-diameter - submission-2

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-2.swift
@@ -0,0 +1,74 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func diameterOfBinaryTree(_ root: TreeNode?) -> Int {
+        var diameter = Int.min
+
+        func dfs(_ root: TreeNode?) -> Int {
+            // base case
+            guard let root else { return 0 }
+
+            // calculate left and right heights
+            let leftHeight = dfs(root.left)
+            let rightHeight = dfs(root.right)
+
+            // compute diameter
+            let height = leftHeight + rightHeight
+            diameter = max(diameter, height)
+
+            // return 1 + largest child
+            return 1 + max(leftHeight, rightHeight)
+        }
+
+        _ = dfs(root)
+
+        return diameter
+    }
+
+    /*
+        diameter == length of the longest path
+
+                B
+
+                    A
+                       C == 6
+                    D       F
+                E               G
+                                    H
+
+
+                    A
+                B       E
+            C
+        D
+
+
+        observations
+        - diameter of a given root = 1 + diameter(root.left) + diameter(root.right)
+        - but parent can only pick one of the child's children
+            i.e B A C D E   or   B A C F G H 
+        - so each root should return it's larger child
+
+        algo
+        - DFS through tree
+        - at each node compute the diameter = 1 + diameter(root.left) + diameter(root.right)
+        - compare that to a global max
+        - pass up the 1 + length of the largest child 
+
+            time O(N)
+            space O(N) (call stack)
+
+    */
+}
