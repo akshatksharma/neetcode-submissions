37de3d1fe9fd2b6fe69a598fafc55240d90d03fe
Akshat Sharma <akshatksharma@wustl.edu>
2026-05-24 11:03:34 -0400

    Add: longest-increasing-subsequence - submission-45

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-45.swift
@@ -0,0 +1,62 @@
+class Solution {
+    func lengthOfLIS(_ nums: [Int]) -> Int {
+        return LIS(nums).count
+    }
+
+    func LIS(_ nums: [Int]) -> [Int] {
+        var dp = nums.map { [$0] } // longest increasing subsequence starting at i
+
+        for startIdx in stride(from: nums.count-1, through: 0, by: -1) {
+            for i in startIdx+1..<nums.count {
+                if nums[i] > nums[startIdx], 
+                   1 + dp[i].count > dp[startIdx].count {
+                    dp[startIdx] = [nums[startIdx]] + dp[i]
+                }
+            }
+        }
+        
+        var ans = [Int]()
+        for arr in dp {
+            if arr.count > ans.count {
+                ans = arr
+            }
+        }
+
+        return ans
+    }
+
+    func LISWithBacktrack(_ nums: [Int]) -> [Int] {
+        var ans = [Int]()
+        var currentAns = [Int]()
+
+        func backtrack(_ startIdx: Int) {
+            if currentAns.count > ans.count {
+                ans = currentAns
+            }
+
+            for i in startIdx..<nums.count {
+                if currentAns.isEmpty || nums[i] > currentAns.last! {
+                    currentAns.append(nums[i])
+                    backtrack(i + 1)
+                    currentAns.removeLast()
+                }
+            }
+        }
+
+        backtrack(0)
+        
+        return ans
+    }
+
+
+    /*
+
+        [7, 5, 3, 8, 9]
+
+                                  _
+         7           5                    3                   8         9 
+    8       9      8    9              8     9             9         
+ 9               9                   9
+
+    */
+}
