5c017f5226a7a497dc4dfef638d78672e2d8c822
Akshat Sharma <akshatksharma@wustl.edu>
2026-05-25 00:13:57 -0400

    Add: spiral-matrix - submission-8

---
+++ Data Structures & Algorithms/spiral-matrix/submission-8.swift
@@ -0,0 +1,42 @@
+class Solution {
+    func spiralOrder(_ matrix: [[Int]]) -> [Int] {
+        guard !matrix.isEmpty, !matrix[0].isEmpty else { return [] }
+        
+        var ans = [Int]()
+        var top = 0
+        var bottom = matrix.count
+        var left = 0
+        var right = matrix[0].count
+        
+        while top < bottom && left < right {
+            // 1. Move Right: (top row)
+            // Use stride or check to ensure range is valid
+            for col in left..<right {
+                ans.append(matrix[top][col])
+            }
+            top += 1
+            
+            // 2. Move Down: (right column)
+            for row in top..<bottom {
+                ans.append(matrix[row][right - 1])
+            }
+            right -= 1
+
+            guard top < bottom && left < right else { break }
+            
+            // 3. Move Left: (bottom row)
+            for col in stride(from: right - 1, through: left, by: -1) {
+                ans.append(matrix[bottom - 1][col])
+            }
+            bottom -= 1
+            
+            // 4. Move Up: (left column)
+            for row in stride(from: bottom - 1, through: top, by: -1) {
+                ans.append(matrix[row][left])
+            }
+            left += 1
+        }
+        
+        return ans
+    }
+}
