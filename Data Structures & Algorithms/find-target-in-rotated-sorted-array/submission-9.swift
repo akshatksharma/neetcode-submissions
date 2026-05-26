22a04bcd9f639945a0f97c216947c9516dfc676f
Akshat Sharma <akshatksharma@wustl.edu>
2026-05-25 23:26:10 -0400

    Add: find-target-in-rotated-sorted-array - submission-9

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-9.swift
@@ -0,0 +1,30 @@
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        guard nums.count > 1 else { return nums[0] == target ? 0 : -1 }
+
+        var lo = 0
+        var hi = nums.count - 1
+
+        while lo <= hi {
+            let mid = (lo + hi) / 2
+            
+            if nums[mid] == target {
+                return mid
+            } else if nums[mid] < nums[hi] { // right half is sorted
+                if (nums[mid]...nums[hi]).contains(target) {
+                    lo = mid + 1
+                } else {
+                    hi = mid - 1
+                }
+            } else { // left half is sorted
+                if (nums[lo]...nums[mid]).contains(target) {
+                    hi = mid - 1
+                }  else {
+                    lo = mid + 1
+                }
+            }
+        }
+
+        return -1
+    }
+}
