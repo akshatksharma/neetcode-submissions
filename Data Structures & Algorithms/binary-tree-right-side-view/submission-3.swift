e4631606c23f8ffbad90d6416a69d0eac4483b13
Akshat Sharma <akshatksharma@wustl.edu>
2026-05-26 11:49:03 -0400

    Add: binary-tree-right-side-view - submission-3

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-3.swift
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+
+    /*
+    - empty tree -> yes
+
+
+        A
+    C        B
+        D
+
+        _
+    */
+
+
+    func rightSideView(_ root: TreeNode?) -> [Int] {
+        var queue = [TreeNode]()
+        var ans = [Int]()
+
+        guard let root else { return [] }
+
+        queue.append(root)
+
+        while !queue.isEmpty {
+            let level = queue.count
+
+            for i in 0..<level {
+                let node = queue.removeFirst()
+                
+                if i == level - 1 {
+                    ans.append(node.val)
+                }
+
+                if let leftNode = node.left {
+                    queue.append(leftNode)
+                }
+
+                if let rightNode = node.right {
+                    queue.append(rightNode)
+                }
+            }
+        }
+
+        return ans
+    }
+}
